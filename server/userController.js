f29110539c7a4605b48b665b976b3bd7bdd51135
Ahmed Jasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2024-05-26 18:28:26 +0200

    kontakt

--- server/userController.js
+++ server/userController.js
@@ -6,16 +6,13 @@ const UserController = {
     try {
       const { name, email, password } = req.body;
       
-      // Hash the password
       const hashedPassword = await bcrypt.hash(password, 10);
       
       const user = new User({ name, email, password: hashedPassword });
       await user.save();
       
-      // Set user ID in session as 'id' instead of '_id'
       req.session.user = { id: user._id, name: user.name, email: user.email };
       
-      // Redirect the user to the home page after successful registration
       res.redirect('/home');
     } catch (error) {
       if (error.code === 11000) {
@@ -30,15 +27,12 @@ const UserController = {
     try {
       const { email, password } = req.body;
 
-      // Find the user by email
       const user = await User.findOne({ email });
 
-      // If user not found or password doesn't match, return error
       if (!user || !(await bcrypt.compare(password, user.password))) {
         return res.render('login', { error: "Netacna email adresa ili sifra." });
       }
 
-      // If user is found and password matches, set session and redirect to home
       req.session.user = user;
       res.redirect('/home');
     } catch (error) {
@@ -47,10 +41,8 @@ const UserController = {
   },
   async logout(req, res) {
     try {
-      // Clear the user session
       req.session.destroy();
 
-      // Redirect the user to the login page (or any other page)
       res.redirect('/home');
     } catch (error) {
       res.status(500).send(error);
